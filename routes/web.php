eeb651eebaa0dd33c00d76b21ef088891f5db5c9
setthawatsaha <setthawat.scs59@ubru.ac.th>
2018-03-07 16:02:26 +0700

    resume

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello', function () {
-    return view('hello');
+Route::get('resume', function () {
+    return view('resume');
 });
